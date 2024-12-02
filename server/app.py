7826c4d12dcadc40fe9ef0ffc797905aa2ab6e37
Branden Vongphakdy <vbranden_@hotmail.com>
2024-12-02 14:37:07 -0500

    update proxy

--- server/app.py
+++ server/app.py
@@ -3,6 +3,7 @@ import os
 from flask_cors import CORS
 from db import init_app # Import the database utility
 from routes.lottery_numbers import lottery_numbers_bp
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 ROOT_DIR = os.path.abspath(os.curdir)
 
@@ -11,6 +12,15 @@ def create_app():
 
     # Database configuration
     app.config['DATABASE'] = os.path.join(ROOT_DIR, 'lottery.sqlite')
+
+    # Add ProxyFix middleware with Nginx as single proxy
+    app.wsgi_app = ProxyFix(
+        app.wsgi_app,
+        x_for=1,      # Number of proxies setting X-Forwarded-For
+        x_proto=1,    # Number of proxies setting X-Forwarded-Proto
+        x_host=1,     # Number of proxies setting X-Forwarded-Host
+        x_prefix=1    # Number of proxies setting X-Forwarded-Prefix
+    )
 
     # Initialize database-related functionality
     init_app(app)
